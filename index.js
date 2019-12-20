6ee8cc2105fc3d816fa5fb3de6188a0305b7131e
mannurajakm <59005048+mannurajakm@users.noreply.github.com>
2019-12-20 12:02:18 +0530

    file change

--- index.js
+++ index.js
@@ -1,28 +1,31 @@
-const express = require("express");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-
-const app = express();
-app.use(cors());
-app.use(bodyParser.json());
-
-const port = 4000;
-
-app.get("/", (req, res) => res.send("Hello World!"));
-
-const dummyDb = { subscription: 'mannu' }; //dummy in memory store
-
-const saveToDatabase = async subscription => {
-  // Since this is a demo app, I am going to save this in a dummy in memory store. Do not do this in your apps.
-  // Here you should be writing your db logic to save it.
-  dummyDb.subscription = subscription;
+const check = () => {
+  if (!("serviceWorker" in navigator)) {
+    throw new Error("No Service Worker support!");
+  }
+  if (!("PushManager" in window)) {
+    throw new Error("No Push API Support!");
+  }
 };
 
-// The new /save-subscription endpoint
-app.post("/save-subscription", async (req, res) => {
-  const subscription = req.body;
-  await saveToDatabase(subscription); //Method to save the subscription to Database
-  res.json({ message: "success" });
-});
+const registerServiceWorker = async () => {
+  const swRegistration = await navigator.serviceWorker.register("service.js");
+  return swRegistration;
+};
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+const requestNotificationPermission = async () => {
+  const permission = await window.Notification.requestPermission();
+  // value of permission can be 'granted', 'default', 'denied'
+  // granted: user has accepted the request
+  // default: user has dismissed the notification permission popup by clicking on x
+  // denied: user has denied the request.
+  if (permission !== "granted") {
+    throw new Error("Permission not granted for Notification");
+  }
+};
+
+const main = async () => {
+  check();
+  const swRegistration = await registerServiceWorker();
+  const permission = await requestNotificationPermission();
+};
+// main(); we will not call main in the beginning.
